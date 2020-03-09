a2a5b3438ad3fc667fccd7b81575e28db4c2cc85
Paulo Matos <pmatos@linki.tools>
2020-03-09 22:11:17 +0100

    Remember me script and job

---
+++ rememberme.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for html in `find public -name '*.html'`; do
+    fpath=`echo $html | cut -d '/' -f2-`
+    url="https://racket-news/$fpath"
+    json="{ \"snapshotURL\": \"$url\" }"
+    response=$(curl -s -H "Content-Type: application/json" --request POST --data "$json" https://dawn-rain-4cff.bkardell.workers.dev/)
+    echo "Remembering $url"
+    echo "    Response: $response"
+done
