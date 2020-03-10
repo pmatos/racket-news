2207b1f66b24f29442aa0d96b33f25a83ba84b9e
Paulo Matos <pmatos@linki.tools>
2020-03-10 15:50:48 +0100

    Fix rememberme script

--- rememberme.sh
+++ rememberme.sh
@@ -2,7 +2,7 @@
 
 for html in `find public -name '*.html'`; do
     fpath=`echo $html | cut -d '/' -f2-`
-    url="https://racket-news/$fpath"
+    url="https://racket-news.com/$fpath"
     json="{ \"snapshotURL\": \"$url\" }"
     response=$(curl -s -H "Content-Type: application/json" --request POST --data "$json" https://dawn-rain-4cff.bkardell.workers.dev/)
     echo "Remembering $url"
