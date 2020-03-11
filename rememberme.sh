12781116a4b935f0b4d050d1c958d8d9db3aba5a
Paulo Matos <pmatos@linki.tools>
2020-03-11 21:45:44 +0100

    Improve remember me using the save snapshot from archive

--- rememberme.sh
+++ rememberme.sh
@@ -1,10 +1,15 @@
-#! /bin/bash
+#! /bin/bash -x
 
+i=0
 for html in `find public -name '*.html'`; do
+    i=$(( i + 1 ))
+    if [[ $i == 15 ]]; then
+	i=0
+	sleep 60
+    fi
     fpath=`echo $html | cut -d '/' -f2-`
     url="https://racket-news.com/$fpath"
-    json="{ \"snapshotURL\": \"$url\" }"
-    response=$(curl -s -H "Content-Type: application/json" --request POST --data "$json" https://dawn-rain-4cff.bkardell.workers.dev/)
+    response=$(curl -s --request GET https://web.archive.org/save/$url | tail -n 7 | head -n 2)
     echo "Remembering $url"
     echo "    Response: $response"
 done
